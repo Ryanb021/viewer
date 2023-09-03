10d7dca91798476ff243499e8d810004c1a76f38
Ryan Bagan <darcknight1980@gmail.com>
2023-09-03 10:55:23 -0700

    fixed

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -15,7 +15,7 @@ const ChannelDetail = () => {
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]));
 
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items[0]));
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items));
   }, [id])
 
   return (
@@ -32,7 +32,7 @@ const ChannelDetail = () => {
       </Box>
       <Box display="flex" p="2">
         <Box sx={{ mr: { sm: '100px' } }} />
-        {/* <Videos videos={videos} /> */}
+        <Videos videos={videos} />
       </Box>
     </Box>
   )
